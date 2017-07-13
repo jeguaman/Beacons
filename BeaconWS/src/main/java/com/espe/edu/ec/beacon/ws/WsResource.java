5ab667d1a39d6212cc71004d0a87bf7b9fb0b84c
Jose Guaman <jeguaman1@gmail.com>
2017-07-12 23:11:54 -0500

    se cambia el Object a String en el WSResource y cada metodo respectivamente se agrega en el pom del EJB Gson

--- BeaconWS/src/main/java/com/espe/edu/ec/beacon/ws/WsResource.java
+++ BeaconWS/src/main/java/com/espe/edu/ec/beacon/ws/WsResource.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.espe.edu.ec.beaconws;
+package com.espe.edu.ec.beacon.ws;
 
 import com.espe.edu.ec.services.ws.RestService;
 import com.espe.edu.ec.services.ws.WSResponse;
